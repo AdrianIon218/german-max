84aa557fa72d908996adb04e6b6dd087253a1c84
AdrianIon218 <ionadrian218@yahoo.com>
2023-11-18 23:39:46 +0200

    testing the router in vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <BrowserRouter>
