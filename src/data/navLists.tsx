cdd76078af7214c1526b046e445a3931b9a9903b
AdrianIon218 <ionadrian218@yahoo.com>
2023-12-12 18:51:38 +0200

    worked on media query for mobiles

--- src/data/navLists.tsx
+++ src/data/navLists.tsx
@@ -2,12 +2,12 @@ export const navListNotLogedIn = [
   { btn: "Pagina principală", to: "/", icon: "fas fa-home" },
   { btn: "Autentificare", to: "/login", icon: "fas fa-pen" },
   { btn: "Înregistrare", to: "/signup", icon: "far fa-clipboard" },
-  { btn: "Suport", to: "/contacts", icon: "far fa-envelope-open" },
+  { btn: "Suport", to: "/support", icon: "far fa-envelope-open" },
 ];
 
 export const navListLogedIn = [
   { btn: "Pagina principală", to: "/", icon: "fas fa-home" },
   { btn: "Deconectare", to: "/", icon: "fas fa-door-closed" },
   { btn: "Cursuri", to: "/courses", icon: "far fa-clipboard" },
-  { btn: "Suport", to: "/contacts", icon: "far fa-envelope-open" },
+  { btn: "Suport", to: "/support", icon: "far fa-envelope-open" },
 ];
